cfd0f393f6648ebbdd97e77470ea7f955c05a1bb
ramintheredmn <redcrossprog@gmail.com>
2025-04-27 17:11:04 +0330

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1556327366"
+    serviceWorkerVersion: "3878480969"
   }
 });
